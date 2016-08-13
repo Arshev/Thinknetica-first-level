a407a0758f91abc750d5a7b8b9ea22e5686fb877
Artem Shevchenko <ashev08@gmail.com>
2016-08-13 12:58:03 +0200

    Add name in else

--- weight.rb
+++ weight.rb
@@ -8,5 +8,5 @@ result = weight - 110
 if result < 0
 	puts 'Ваш вес уже оптимальный'
 else 
-	puts "Твой вес равен #{weight}, это много! Пора худеть!"
+	puts "#{name}, твой вес равен #{weight}, это много! Пора худеть!"
 end	
