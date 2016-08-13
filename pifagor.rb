784ffe6b4c4bbda7a81d6baf73225e49349f1e35
Artem Shevchenko <ashev08@gmail.com>
2016-08-13 14:07:43 +0200

    Find out the rectangular triangle

---
+++ pifagor.rb
@@ -0,0 +1,19 @@
+puts 'Введите три стороны треуголника:'
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+c = gets.chomp.to_i
+
+arr = Array.new
+arr << a
+arr << b
+arr << c
+arr.sort!.reverse!
+hypotenuse = arr[0]
+cat1 = arr[1]
+cat2 = arr[2]
+
+if cat1**2 + cat2**2 == hypotenuse**2
+	puts 'Треуголник прямоуголный!'
+else
+	puts 'Треуголник НЕ прямоуголный!'
+end
