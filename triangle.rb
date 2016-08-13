c7db507bfe69b5f8590e03335d33b979a62d7a79
Artem Shevchenko <ashev08@gmail.com>
2016-08-13 21:34:10 +0200

    Bug fix

--- triangle.rb
+++ triangle.rb
@@ -1,6 +1,6 @@
 puts 'Введите основание треуголника:'
 base = gets.chomp.to_f
 puts 'Введите высоту треуголника:'
-height = gets.chomp.to_i
+height = gets.chomp.to_f
 area = 0.5 * base * height
 puts "Площадь треуголника равна: #{area}"
