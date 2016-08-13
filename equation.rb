3acf5f7a3c60ac9e660a0ea909b8901783c58161
Artem Shevchenko <ashev08@gmail.com>
2016-08-13 15:32:00 +0200

    Condition if discriminant > 0

--- equation.rb
+++ equation.rb
@@ -7,11 +7,12 @@ p discriminant = b**2 - 4 * a * c
 
 if discriminant < 0
 	puts 'Корней нет!'
+
 elsif  discriminant == 0
 	radical = Math.sqrt(discriminant)
 	puts "Дискриминант: #{discriminant} и x1: #{х1 = (-b + radical) / ( 2.0 * a )}"
 
 elsif discriminant > 0
-	puts 'выводим дискриминант и 2 корня'
-
+	radical = Math.sqrt(discriminant)
+	puts "Дискриминант: #{discriminant}, x1: #{х1 = (-b + radical) / ( 2.0 * a )}, x2: #{х2 = (-b - radical) / ( 2.0 * a )}"
 end 	
