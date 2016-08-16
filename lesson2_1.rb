c7ed9d00a06324605e43cfadb217a3d05482c3a1
Artem Shevchenko <ashev08@gmail.com>
2016-08-16 11:53:23 +0200

    Choose the vowels from the array

--- lesson2_1.rb
+++ lesson2_1.rb
@@ -32,25 +32,23 @@
 
 #Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
 hh = {}
-	arr1 = ("a".."z").to_a
-	arr2 = (1..28).to_a
-	vol = ["a", "e", "i", "o", "u"]
+vol = ["a", "e", "i", "o", "u"]
+('a'..'z').each_with_index do |key, value|
+	value += 1
 	index = 0
-	arr3 = arr1 & vol
-	
-while true 
-	
-	key = arr1[index]
-	value = arr2[index]
-	p index = index + 1
-
-	hh[key] = value
-
-	if index == 26
-		break
+	loop do
+		index += 1
+		if index == 26
+			break
+		end
+		vol.each do |letter|
+			x = letter.to_s
+			if key.include?(x)
+	   		hh[key] = value
+			end
+		end
+		
 	end
 end
-
-p arr3
-
 	
+p hh
