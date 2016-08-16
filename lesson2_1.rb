9b2c0437dfe6be9909c8b5246d9180610c8383d0
Artem Shevchenko <ashev08@gmail.com>
2016-08-16 14:10:43 +0200

    Make a leap year

--- lesson2_1.rb
+++ lesson2_1.rb
@@ -66,6 +66,10 @@ month_hh = {january: 31, february: 28, march: 31, april: 30, may: 31, june: 30, 
 
 month_index = {january: 1, february: 2, march: 3, april: 4, may: 5, june: 6, july: 7, august: 8, september: 9, october: 10, november: 11, december: 12}
 
+if year % 4 == 0
+	month_hh[:february] = 29
+end
+
 days = month_hh.values
 
 month_days = []
