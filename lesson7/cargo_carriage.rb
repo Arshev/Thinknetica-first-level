e3948137a5db08d484c665bf4cbe0cc03271fe0a
Artem Shevchenko <ashev08@gmail.com>
2016-09-02 16:24:05 +0200

    Add methods for cargo carriage

--- lesson7/cargo_carriage.rb
+++ lesson7/cargo_carriage.rb
@@ -1,7 +1,21 @@
 class CargoCarriage < Carriage
 
-  def initialize
+  def initialize(volume)
     @type = 'Cargo'
+    @volume = volume
+    @dif_volume = volume
   end 
+
+  def take_volume(quantity)
+    @dif_volume -= quantity
+  end
+
+  def occupied_volume 
+    @occupied_volume = @volume - @dif_volume
+  end
+
+  def free_volume 
+    @volume - @occupied_volume
+  end
 
 end  
