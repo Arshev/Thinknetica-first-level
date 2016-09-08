2c1e31dd8aeeb94a0b72a6e595c65d0f112c63a1
Artem Shevchenko <ashev08@gmail.com>
2016-09-08 12:06:04 +0200

    Make strong_attr_accessor

--- lesson9/Acessors.rb
+++ lesson9/Acessors.rb
@@ -3,19 +3,21 @@ module Accessors
     base.extend ClassMethods
   end
 
-  def attr_accessor_with_history(*attributes)
-    attributes.each do |attribute|
-      attribute_name = "@#{attribute}".to_sym
-      define_method(attribute.to_sym) { instance_variable_get(attribute_name) }
-      define_method("#{attribute}=".to_sym) do |value|
-        instance_variable_set(attribute_name, value)
-        if @history[attribute_name]
-          @history[attribute_name] << value
-        else
-          @history[attribute_name] = [value]
+  module ClassMethods
+    def attr_accessor_with_history(*attributes)
+      attributes.each do |attribute|
+        attribute_name = "@#{attribute}".to_sym
+        define_method(attribute.to_sym) { instance_variable_get(attribute_name) }
+        define_method("#{attribute}=".to_sym) do |value|
+          instance_variable_set(attribute_name, value)
+          if @history[attribute_name]
+            @history[attribute_name] << value
+          else
+            @history[attribute_name] = [value]
+          end
         end
+        define_method("#{attribute}_history") { instance_variable_get @history[attribute_name] }
       end
-      define_method("#{attribute}_history") { instance_variable_get @history[attribute_name] }
     end
   end
 
